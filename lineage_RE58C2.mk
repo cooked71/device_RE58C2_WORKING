fbbbbf9d329b58dda4f482d6f45a3989d81d7a89
cooked71 <53344663+cooked71@users.noreply.github.com>
2025-08-28 12:09:08 +0200

    add  generic_system_arm64.mk

--- lineage_RE58C2.mk
+++ lineage_RE58C2.mk
@@ -4,6 +4,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+
+
+# Platform
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_system_arm64.mk)
+
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
