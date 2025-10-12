86179537247b11d7994603d1905e7cfc8ceb9d75
cooked71 <53344663+cooked71@users.noreply.github.com>
2025-10-12 19:30:20 +0200

    cccc

--- device.mk
+++ device.mk
@@ -699,8 +699,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.security.keymint.unisoc.trusty.manifest \
     android.hardware.gatekeeper@1.0-service.trusty.manifest \
-    trusty.default.manifest \
-    android.system.keystore2.manifest
+    trusty.default.manifest 
+  #  android.system.keystore2.manifest
 
 
 
