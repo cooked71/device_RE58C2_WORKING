8ab006d6148d7468e756becc38648e42913e9740
cooked71 <53344663+cooked71@users.noreply.github.com>
2025-10-28 22:39:04 +0100

    ddd

--- device.mk
+++ device.mk
@@ -410,11 +410,11 @@ PRODUCT_PACKAGES_OVERRIDES += \
     libunisocvold  \
     libphoenix_native  \
     android.hardware.health.storage@1.0  \
-    android.system.keystore2-V2-ndk  \
     libkeymint_support  \
     android.hardware.boot@1.0-1.2-impl  \
     android.hardware.boot@1.0 
     #android.hardware.health.storage-V1-ndk  
+    #android.system.keystore2-V2-ndk
 
 
 # Remove AOSP vold to prevent conflicts
