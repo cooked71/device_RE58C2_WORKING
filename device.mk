290ea0683e5fdcce5d1b944d7ac6c317e933b3b9
cooked71 <53344663+cooked71@users.noreply.github.com>
2025-10-28 21:27:53 +0100

    xxx

--- device.mk
+++ device.mk
@@ -413,8 +413,8 @@ PRODUCT_PACKAGES_OVERRIDES += \
     android.system.keystore2-V2-ndk  \
     libkeymint_support  \
     android.hardware.boot@1.0-1.2-impl  \
-    android.hardware.boot@1.0 \
-    android.hardware.health.storage-V1-ndk  
+    android.hardware.boot@1.0 
+    #android.hardware.health.storage-V1-ndk  
 
 
 # Remove AOSP vold to prevent conflicts
