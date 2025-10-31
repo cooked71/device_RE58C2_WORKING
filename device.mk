b65840eca1f4ab653e007c5a6c2aab6a7a369351
cooked71 <53344663+cooked71@users.noreply.github.com>
2025-10-31 03:19:26 +0100

    CLEAN FRAGMENTS

--- device.mk
+++ device.mk
@@ -43,15 +43,9 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/realme/RE58C2/proprietary/p
 PRODUCT_ENFORCE_VINTF_MANIFEST := true
 DEVICE_MANIFEST_FILE +=  vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/ai_engine-default.xml \
 vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/android.hardware.biometrics.fingerprint@2.1-service.xml \
-vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/android.hardware.cas@1.2-service.xml \
 vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/android.hardware.drm-service.clearkey.xml \
-vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/android.hardware.gatekeeper@1.0-service.trusty.xml \
-vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/android.hardware.health-service.example.xml \
 vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/android.hardware.security.keymint@2.0-unisoc.service.trusty.xml \
-vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/android.hardware.sensors-multihal.xml \
 vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/android.hardware.thermal@2.0-service.xml \
-vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/android.hardware.usb-service.example.xml \
-vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/android.hardware.wifi@1.0-service.xml \
 vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/android.hardware.wifi.hostapd.xml \
 vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/android.hardware.wifi.supplicant.xml \
 vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/bluetooth_audio.xml \
@@ -66,7 +60,6 @@ vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/manifest_media_c2_V1_
 vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/manifest_oplus_performance.xml \
 vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/memtrack.xml \
 vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/network-default.xml \
-vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/power.stats-default.xml \
 vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/rebootescrow-default.xml \
 vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/soter_default.xml \
 vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/trusty-default.xml \
